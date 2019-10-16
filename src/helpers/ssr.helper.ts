f3dc8a774078d96b68f6e1a2346f311336e29a0b
Jimmy <jackzard@yahoo.com>
2019-10-16 21:14:13 +0800

    fix(SSR): change detection is browser only by window & document

--- src/helpers/ssr.helper.ts
+++ src/helpers/ssr.helper.ts
@@ -1,8 +1,5 @@
 /**
  * Detect is browser or not
  */
-declare var process
-
 export const IsBrowser = typeof window !== 'undefined'
 	&& typeof document !== 'undefined'
-	&& typeof process === 'object'
